64e30235874f486061578abe6d38fe7e2de6ad14
Scott Vojik <scott.vojik@gmail.com>
2019-01-16 13:02:26 -0800

    refactored app, created PostsPage

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,53 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import dummyData from './Components/dummy-data'
-import SearchBar from './Components/SearchBar/SearchBar'
-import PostContainer from './Components/PostContainer/PostContainer'
 
+import PostPage from "./Components/PostContainer/PostsPage"
 import './Components/SearchBar/SearchBar.css'
 
 
-
-
-class App extends Component {
-  constructor () {
-    super();
-    this.state = {
-      dataList: [],
-      searchText: '',
-      searchPosts: []
-    };
-  }
-
-  componentDidMount() {
-    this.setState({ dataList: dummyData });
-  }
-
-  handleSearch = ev => {
-    this.setState({
-      searchText: ev.target.value
-    });
-  };
-
-  searchUsers = ev => {
-    ev.preventDefault();
-    this.setState({
-      searchPosts: this.state.dataList.filter((userSearch) => {
-      return userSearch.username.includes(this.state.searchText);
-      })
-    });
-  }
-
-
-
-  render() {
-    return (
-      <div className="App">
-        <SearchBar search={this.searchUsers} handleSearch={this.handleSearch}/>
-        <PostContainer instaDataList={this.state.searchPosts.length > 0 ? this.state.searchPosts : this.state.dataList}/>
-      </div>
-    );
-  }
+function App () {
+  
+  return (
+    <>
+      <PostPage />
+    </>
+  )
+  
 }
 
 export default App;
---
+++ instagram/src/Components/PostContainer/PostsPage.js
@@ -0,0 +1,47 @@
+import React from "react"
+import dummyData from '../dummy-data'
+import SearchBar from '../SearchBar/SearchBar'
+import PostContainer from './PostContainer'
+
+class PostsPage extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      dataList: [],
+      searchText: '',
+      searchPosts: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ dataList: dummyData });
+  }
+
+  handleSearch = ev => {
+    this.setState({
+      [ev.target.name]: ev.target.value
+    });
+  };
+
+  searchUsers = ev => {
+    ev.preventDefault();
+    this.setState({
+      searchPosts: this.state.dataList.filter((userSearch) => {
+      return userSearch.username.includes(this.state.searchText);
+      })
+    });
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <SearchBar search={this.searchUsers} handleSearch={this.handleSearch}/>
+        <PostContainer instaDataList={this.state.searchPosts.length > 0 ? this.state.searchPosts : this.state.dataList}/>
+      </div>
+    );
+  }
+}
+
+export default PostsPage
